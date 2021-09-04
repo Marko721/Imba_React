b8cedff9dab4307d40b33a330a198efd41921613
Marko721 <popmark1002@gmail.com>
2021-09-04 13:25:43 +0200

    start boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
+import Layout from "./Components/Layout";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import Nurses from "./Pages/Nurses";
+import Facilities from "./Pages/Facilities";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff8300",
+    },
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/Nurses">
+              <Nurses />
+            </Route>
+            <Route path="/Facilities">
+              <Facilities />
+            </Route>
+            <Route path="/About-us">
+              <About />
+            </Route>
+            <Route path="/Contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Layout.js
@@ -0,0 +1,9 @@
+import { makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import { useHistory, useLocation } from "react-router";
+
+const Layout = ({ children }) => {
+  return <div></div>;
+};
+
+export default Layout;
