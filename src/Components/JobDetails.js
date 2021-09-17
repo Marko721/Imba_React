e49cd3d06454837a2a84e5e2be739513a5ce0547
Marko721 <popmark1002@gmail.com>
2021-09-17 17:07:28 +0200

    refactor

--- src/Components/JobDetails.js
+++ src/Components/JobDetails.js
@@ -1,4 +1,4 @@
-import { Paper, Typography } from "@material-ui/core";
+import { Button, Paper, Typography } from "@material-ui/core";
 import { CallMissedSharp } from "@material-ui/icons";
 import { useParams } from "react-router";
 import Data from "../Components/Data";
@@ -17,6 +17,11 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "100%",
     maxHeight: "100%",
     borderRadius: 5,
+  },
+  jobRequirementsWrapper: {
+    boxSizing: "border-box",
+    height: "100%",
+    padding: 20,
   },
 }));
 
@@ -38,11 +43,24 @@ const JobDetails = () => {
       <Container>
         <Paper elevation={0} className={classes.jobDetailsWrapper}>
           <Typography variant="h4">{Job.Facility}</Typography>
-          <Grid container>
+          <Grid container spacing={5}>
             <Grid item xs={12} sm={8} md={8}>
               <img className={classes.img} alt="job_search" src={Job.Image} />
             </Grid>
             <Grid item xs={12} sm={4} md={4}>
+              <Paper elevation={2} className={classes.jobRequirementsWrapper}>
+                <Typography variant="h4">Application Requirements</Typography>
+                <Typography gutterBottom>MA License</Typography>
+                <Typography gutterBottom>BLS Certification</Typography>
+                <Typography gutterBottom>2 Years of RN Experience</Typography>
+                <Typography gutterBottom>Oncology Specialty</Typography>
+                <Button variant="contained" color="secondary">
+                  Apply to job
+                </Button>
+              </Paper>
+            </Grid>
+
+            <Grid item xs={6}>
               <Typography variant="h4" color="secondary" gutterBottom>
                 {Job.Description} • {Job.City}, {Job.State}
               </Typography>
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -11,6 +11,7 @@ const useStyles = makeStyles((theme) => {
       // backgroundColor: "#030303",
       // width: "80vw",
       // padding: 5,
+      boxSizing: "border-box",
     },
   };
 });
