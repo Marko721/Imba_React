95fd7cacce11120eab41389f5bf90afa1de87943
Marko721 <popmark1002@gmail.com>
2021-09-17 13:24:38 +0200

    Job card component refactor

---
+++ src/Components/JobCard.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  jobsContainer: {
+    padding: theme.spacing(2),
+    maxWidth: "100%",
+    "&:hover": {
+      backgroundColor: "rgba(145, 195, 221, .2)",
+    },
+  },
+  image: {
+    // width: 200,
+    height: "100%",
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    borderRadius: 5,
+  },
+  iconTextContainer: {
+    display: "flex",
+  },
+  iconText: {
+    marginLeft: 5,
+  },
+  interestedButton: {
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+}));
+
+const JobCard = ({ Job }) => {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.jobsContainer} elevation={1}>
+      <Grid container spacing={2} alignContent="center">
+        <Grid item xs={12} sm={4} md={3}>
+          <ButtonBase className={classes.image}>
+            <img className={classes.img} alt="job_search" src={Job.Image} />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm={8} md={9} container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs>
+              <Typography gutterBottom>{Job.Facility}</Typography>
+              <Typography variant="h4" color="secondary" gutterBottom>
+                {Job.Description} • {Job.City}, {Job.State}
+              </Typography>
+
+              <div className={classes.iconTextContainer}>
+                <i class="fas fa-user-md"></i>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  className={classes.iconText}
+                >
+                  {Job.Specialty}
+                </Typography>
+              </div>
+              <div className={classes.iconTextContainer}>
+                <i class="fas fa-hourglass-half"></i>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  className={classes.iconText}
+                >
+                  {Job.Shift}
+                </Typography>
+              </div>
+              <div className={classes.iconTextContainer}>
+                <i class="far fa-calendar"></i>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  className={classes.iconText}
+                >
+                  {Job.Start}
+                </Typography>
+              </div>
+              <div className={classes.iconTextContainer}>
+                <i class="far fa-calendar-alt"></i>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  className={classes.iconText}
+                >
+                  {Job.Length}
+                </Typography>
+              </div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle1">$3923.00/week</Typography>
+                </Grid>
+                <Grid item xs={6} className={classes.interestedButton}>
+                  <Button variant="contained" color="secondary">
+                    I'm interested
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default JobCard;
--- src/Pages/JobSearch.js
+++ src/Pages/JobSearch.js
@@ -4,13 +4,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import ButtonBase from "@material-ui/core/ButtonBase";
 import Button from "@material-ui/core/Button";
 import Pagination from "@material-ui/lab/Pagination";
 import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import JobCard from "../Components/JobCard";
 
 const useStyles = makeStyles((theme) => ({
   //FILTERS SECTION
@@ -34,35 +33,6 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     marginTop: 25,
     marginBottom: 25,
-  },
-  jobsContainer: {
-    padding: theme.spacing(2),
-    maxWidth: "100%",
-    "&:hover": {
-      backgroundColor: "rgba(145, 195, 221, .2)",
-    },
-  },
-  image: {
-    // width: 200,
-    height: "100%",
-  },
-  img: {
-    margin: "auto",
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%",
-    borderRadius: 5,
-  },
-  iconTextContainer: {
-    display: "flex",
-  },
-  iconText: {
-    marginLeft: 5,
-  },
-  interestedButton: {
-    display: "flex",
-    alignItems: "flex-end",
-    justifyContent: "flex-end",
   },
 
   pagination: {
@@ -408,85 +378,10 @@ const JobSearch = () => {
         </Grid>
       </Paper>
 
-      {/* GRID ITEM */}
+      {/* JOB GRID ITEM */}
       {dbApi.map((Job) => (
         <div className={classes.jobsWrapper} key={Job.id}>
-          <Paper className={classes.jobsContainer} elevation={1}>
-            <Grid container spacing={2} alignContent="center">
-              <Grid item xs={12} sm={4} md={3}>
-                <ButtonBase className={classes.image}>
-                  <img
-                    className={classes.img}
-                    alt="job_search"
-                    src={Job.Image}
-                  />
-                </ButtonBase>
-              </Grid>
-              <Grid item xs={12} sm={8} md={9} container>
-                <Grid item xs container direction="column" spacing={2}>
-                  <Grid item xs>
-                    <Typography gutterBottom>{Job.Facility}</Typography>
-                    <Typography variant="h4" color="secondary" gutterBottom>
-                      {Job.Description} • {Job.City}, {Job.State}
-                    </Typography>
-
-                    <div className={classes.iconTextContainer}>
-                      <i class="fas fa-user-md"></i>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.iconText}
-                      >
-                        {Job.Specialty}
-                      </Typography>
-                    </div>
-                    <div className={classes.iconTextContainer}>
-                      <i class="fas fa-hourglass-half"></i>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.iconText}
-                      >
-                        {Job.Shift}
-                      </Typography>
-                    </div>
-                    <div className={classes.iconTextContainer}>
-                      <i class="far fa-calendar"></i>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.iconText}
-                      >
-                        {Job.Start}
-                      </Typography>
-                    </div>
-                    <div className={classes.iconTextContainer}>
-                      <i class="far fa-calendar-alt"></i>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.iconText}
-                      >
-                        {Job.Length}
-                      </Typography>
-                    </div>
-                    <Grid container>
-                      <Grid item xs={6}>
-                        <Typography variant="subtitle1">
-                          $3923.00/week
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={6} className={classes.interestedButton}>
-                        <Button variant="contained" color="secondary">
-                          I'm interested
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Paper>
+          <JobCard Job={Job} />
         </div>
       ))}
 
