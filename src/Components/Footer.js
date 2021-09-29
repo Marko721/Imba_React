4099bb4cac1bc6438e1328ddd8af60112f024ad8
Marko721 <popmark1002@gmail.com>
2021-09-29 16:28:12 +0200

    Footer working of

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,6 +5,7 @@ import {
   Typography,
   Grid,
   Container,
+  Divider,
 } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -16,8 +17,21 @@ import Logo from "./Images/logo.png";
 const useStyles = makeStyles((theme) => ({
   // FOOTER
   footer: {
-    height: 400,
-    backgroundColor: "rgba(0,0,0,.5)",
+    paddingTop: 30,
+    paddingBottom: 30,
+    backgroundColor: "#3F86AB",
+  },
+  title: {
+    paddingLeft: 16,
+    marginBottom: 5,
+    color: "#fff",
+    fontSize: 18,
+  },
+  listItem: {
+    color: "#fff",
+    "&:hover": {
+      // color: "#676767",
+    },
   },
 }));
 
@@ -46,12 +60,29 @@ const Footer = ({ mobileView }) => {
     },
   ];
 
+  const resources = [
+    {
+      text: "FAQ",
+      path: "/faq",
+    },
+    {
+      text: "Support",
+      path: "/support",
+    },
+    {
+      text: "Contact",
+      path: "/contact",
+    },
+  ];
+
   return (
     <div className={classes.footer}>
       <Container>
-        <Grid container>
+        <Grid container spacing={5}>
           <Grid item xs={12} sm={6} md={3}>
-            <List className={classes.listitemleft}>
+            <List>
+              <Typography className={classes.title}>Links</Typography>
+              <Divider />
               {menuItems.map((item) => (
                 <ListItem
                   key={item.text}
@@ -59,9 +90,25 @@ const Footer = ({ mobileView }) => {
                   onClick={() => history.push(item.path)}
                 >
                   <ListItemText
-                    className={
-                      location.pathname == item.path ? classes.active : null
-                    }
+                    className={classes.listItem}
+                    primary={item.text}
+                  ></ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <List>
+              <Typography className={classes.title}>Resources</Typography>
+              <Divider />
+              {resources.map((item) => (
+                <ListItem
+                  key={item.text}
+                  button
+                  onClick={() => history.push(item.path)}
+                >
+                  <ListItemText
+                    className={classes.listItem}
                     primary={item.text}
                   ></ListItemText>
                 </ListItem>
