104b1b7168d3858a6ec164ed5e78b8763dbdbb43
Marko721 <popmark1002@gmail.com>
2021-11-05 00:37:57 +0100

    Login working of

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 // handle errors
 const handleErrors = (err) => {
